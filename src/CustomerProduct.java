347817b4f9fdbede53fb619d4fdec509ed5fdb3a
Elsherif <yosuefelshref@gmail.com>
2025-10-18 18:30:06 +0300

    Added CustomerProduct.java and EmployeeRole.java

--- src/CustomerProduct.java
+++ src/CustomerProduct.java
@@ -1,2 +1,45 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class CustomerProduct {
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate, boolean paid) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public String lineRepresentation() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+    }
+
+    public String getSearchKey() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter);
+    }
 }
